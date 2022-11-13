613ea4ad19fcff029d1117d5f7d2b3703b9811a9
theSaxena06 <aryanswati2002@gmail.com>
2022-11-14 04:07:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,139 @@
+
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+
+
+// Importing Libraies that we installed using npm
+const express = require("express")
+const app = express()
+const bcrypt = require("bcrypt") // Importing bcrypt package
+const passport = require("passport")
+const initializePassport = require("./passport-config")
+const flash = require("express-flash")
+const session = require("express-session")
+const methodOverride = require("method-override")
+
+//added
+var mongoose = require("mongoose");
+
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+    )
+
+
+
+const users = []
+
+//added
+mongoose.connect("mongodb://localhost:27017/LoginAppdb",{}
+).then(()=>{
+    console.log(`connection successful`);
+}).catch((e)=>{
+    console.log(`no connection`);
+})
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // We wont resave the session variable if nothing is changed
+    saveUninitialized: false
+}))
+app.use(passport.initialize()) 
+app.use(passport.session())
+app.use(methodOverride("_method"))
+
+// Configuring the register post functionality
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+}))
+
+// Configuring the register post functionality
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+
+    try {
+        //added
+        var name = req.body.name;
+        var email = req.body.email;
+        var password = req.body.password;
+        
+        var data = {
+            "name": name,
+            "email" : email,
+            "password" : password
+        }
+        db.collection('users').insertOne(data,(err,collection)=>{
+            if(err){
+                throw err;
+            }
+            console.log("Record Inserted Successfully");
+        });
+
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(), 
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        console.log(users); // Display newly registered in the console
+        res.redirect("/login")
+        
+    } catch (e) {
+        console.log(e);
+        res.redirect("/register")
+    }
+})
+
+// Routes
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render("index.ejs", {name: req.user.name})
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render("register.ejs")
+})
+// End Routes
+
+// app.delete('/logout', (req, res) => {
+//     req.logOut()
+//     res.redirect('/login')
+//   })
+
+app.delete("/logout", (req, res) => {
+    req.logout(req.user, err => {
+        if (err) return next(err)
+        res.redirect("/")
+    })
+})
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login")
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect("/")
+    }
+    next()
+}
+
+app.listen(3000)
